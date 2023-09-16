cb75a02227b0814553ffbdf6f7f54ce1433c2ec7
paragofficial <97843814+paragofficial@users.noreply.github.com>
2023-09-16 17:36:38 +0530

    Update connectDb.js

--- config/connectDb.js
+++ config/connectDb.js
@@ -4,7 +4,8 @@ const colors = require('colors');
 
 const connectDb = async ()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URL);
+        // await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(`mongodb+srv://paragofficial1002:parag@cluster0.6w0ekc6.mongodb.net/expense`);
         console.log(`server running on ${mongoose.connection.host}`.bgCyan.white);
     }catch(error){
         console.log(`${error}.bgRed`);
